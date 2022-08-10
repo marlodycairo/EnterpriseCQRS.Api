d3f3f054a5ce658fe10b601fe5fc7b5d342168aa
juan santander <marlodycairo@hotmail.com>
2022-08-10 16:00:05 -0500

    Creación clase Currency

---
+++ EnterpriseCQRS.Data/Model/Conversiones.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseCQRS.Data.Model
+{
+    public class Conversiones
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+
+        //Rate es el valor unitario
+        public string Rate { get; set; }
+
+        //Currency si es diferente de EUR se hace el cambio de moneda
+        public string Currency { get; set; }
+
+        //Amount corresponde al valor total que vamos a cambiar de moneda segun lo que traiga Currency
+        public string Amount { get; set; }
+
+        //Representa la conversion final a EUR
+        public string CurrencyFinal { get; set; }
+
+    }
+}
---
+++ EnterpriseCQRS.Data/Model/Currency.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseCQRS.Data.Model
+{
+    public enum Currency
+    {
+        AUD = 1,
+        CAD = 2,
+        EUR = 3,
+        USD = 4
+    }
+}
